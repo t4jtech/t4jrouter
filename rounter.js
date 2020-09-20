c7b5aaccf3c912e2b353d8fb85702f4d8104f9a0
Tejas Nai <naitejas@gmail.com>
2020-09-20 14:17:29 +0530

    router

---
+++ rounter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+// Config
+const { routes } = require('./config.json');
+
+
+
+const app = express();
+const privateKey = fs.readFileSync('/home/ubuntu/t4j-ssl/technology4jewelry.com.ey');
+const certificate = fs.readFileSync('/home/ubuntu/t4j-ssl/t4j.crt.pem');
+const ca = fs.readFileSync('/home/ubuntu/t4j-ssl/gd_bundle-g2-g1.crt');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+  
+for (route of routes) {
+    app.use(route.route,
+        proxy({
+            target: route.address,
+            pathRewrite: (path, req) => {
+                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+            }
+        })
+    );
+}
+
+//app.listen(port,hostname, () => {
+    //console.log('Proxy Started');
+//});
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+    });
