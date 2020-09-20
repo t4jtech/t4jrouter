460e26e727fe361df8a0cc48a8af8a3d70f39917
Tejas Nai <naitejas@gmail.com>
2020-09-20 14:36:01 +0530

    deploy

--- rounter.js
+++ rounter.js
@@ -23,16 +23,7 @@ const credentials = {
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
   
-for (route of routes) {
-    app.use(route.route,
-        proxy({
-            target: route.address,
-            pathRewrite: (path, req) => {
-                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
-            }
-        })
-    );
-}
+
 
 //app.listen(port,hostname, () => {
     //console.log('Proxy Started');
