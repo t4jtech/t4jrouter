3363cd632a10ff56ad861044e73b7181a9a3edcb
Tejas Nai <naitejas@gmail.com>
2020-09-29 17:59:53 +0530

    cert

--- rounter.js
+++ rounter.js
@@ -10,9 +10,9 @@ const { routes } = require('./config.json');
 
 
 const app = express();
-const privateKey = fs.readFileSync('/home/ubuntu/t4j-ssl/technology4jewelry.com.key');
-const certificate = fs.readFileSync('/home/ubuntu/t4j-ssl/t4j.crt');
-const ca = fs.readFileSync('/home/ubuntu/t4j-ssl/gd_bundle-g2-g1.crt');
+const privateKey = fs.readFileSync('/home/ubuntu/t4j-ssl/private.pem', 'utf8');
+const certificate = fs.readFileSync('/home/ubuntu/t4j-ssl/t4j.pem', 'utf8');
+const ca = fs.readFileSync('/home/ubuntu/t4j-ssl/gd_bundle-g2-gl.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
@@ -23,16 +23,17 @@ const credentials = {
 //const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
   
-
-
-//app.listen(port,hostname, () => {
-    //console.log('Proxy Started');
-//});
-
-// httpServer.listen(80, () => {
-//     console.log('HTTP Server running on port 80');
-// });
-
-httpsServer.listen(443, () => {
+  
+for (route of routes) {
+    app.use(route.route,
+        proxy({
+            target: route.address,
+            pathRewrite: (path, req) => {
+                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+            }
+        })
+    );
+}
+httpsServer.listen(3000, () => {
     console.log('HTTPS Server running on port 443');
     });
